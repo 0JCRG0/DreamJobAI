626ced6ff591a9dfe4483035eeb55d42838aa3b8
JCRG <juancarlosrg1999@gmail.com>
2023-08-14 09:58:39 -0600

    fixed bug

--- EmbeddingsE5.py
+++ EmbeddingsE5.py
@@ -31,10 +31,11 @@ E5_BASE_TOTAL_JOBS = os.getenv("E5_BASE_TOTAL_JOBS")
 LoggingMain()
 
 #@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
-def embedding_e5_base_v2(batches_to_embed: list[str], batches_ids: list[str], original_descriptions: list[str], chunk_size: int) -> list:
+def embedding_e5_base_v2(batches_to_embed: list[str], batches_ids: list[str], original_timestamps: list[str], original_descriptions: list[str], chunk_size: int) -> list:
     
     INPUT_TEXT = batches_to_embed
     INPUT_IDS = batches_ids
+    INPUT_TIMESTAMPS = original_timestamps
     INPUT_ORIGINAL = original_descriptions
     TOKENIZER = AutoTokenizer.from_pretrained("intfloat/e5-base-v2")
     MODEL = AutoModel.from_pretrained("intfloat/e5-base-v2")
@@ -80,16 +81,17 @@ def embedding_e5_base_v2(batches_to_embed: list[str], batches_ids: list[str], or
     EMBEDDINGS = np.vstack(embeddings_list)
 
     #Timestamps
+    """
     TIMESTAMPS = []
     timestamp = datetime.now()
-    TIMESTAMPS.extend([timestamp] * len(INPUT_IDS))
+    TIMESTAMPS.extend([timestamp] * len(INPUT_IDS)) """
 
     df_data = {
         'id': INPUT_IDS,
         'original': INPUT_ORIGINAL,
         'summary': INPUT_TEXT,
         'embedding': list(EMBEDDINGS),
-        'timestamp': TIMESTAMPS
+        'timestamp': INPUT_TIMESTAMPS
         }
     
     new_data = pd.DataFrame(df_data)
