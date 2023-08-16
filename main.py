e5200eb9286d02798c84bc181a7f64160524001f
JCRG <juancarlosrg1999@gmail.com>
2023-08-15 23:28:18 -0600

    new feature

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from utils.SummariseJob import summarise_job_gpt
 from utils.AsyncSummariseJob import async_summarise_description
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
-from utils.handy import e5_base_v2_query, LoggingGPT4, filter_last_two_weeks, df_to_parquet
+from utils.handy import e5_base_v2_query, filter_last_two_weeks, append_parquet, num_tokens, set_dataframe_display_options
 from tenacity import (
     retry,
     stop_after_attempt,
@@ -50,7 +50,8 @@ GPT_MODEL = "gpt-4"
 Load the embedded file
 """
 
-logging.basicConfig(filename='/Users/juanreyesgarcia/Library/CloudStorage/OneDrive-FundacionUniversidaddelasAmericasPuebla/DEVELOPER/PROJECTS/DreamedJobAI/logs/LoggingGPT4.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='/Users/juanreyesgarcia/Library/CloudStorage/OneDrive-FundacionUniversidaddelasAmericasPuebla/DEVELOPER/PROJECTS/DreamedJobAI/logs/LoggingGPT4.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 
 embeddings_path = E5_BASE_V2_DATA
@@ -62,9 +63,9 @@ df = filter_last_two_weeks(df_unfiltered)
 
 def ids_ranked_by_relatedness_e5(query: str,
     df: pd.DataFrame,
+    min_n: int,
+    top_n: int,
     relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
-    #Modify this to get more jobs
-    top_n: int = 10
 ) -> tuple[list[str], list[float]]:
     
     #the query is embedded using e5
@@ -76,23 +77,19 @@ def ids_ranked_by_relatedness_e5(query: str,
     ]
     ids_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
     ids, relatednesses = zip(*ids_and_relatednesses)
-    return ids[:top_n], relatednesses[:top_n]     
+    return ids[min_n:top_n], relatednesses[min_n:top_n]     
     #Returns a list of strings and relatednesses, sorted from most related to least.
-
-#tiktoken function -> to count tokens
-def num_tokens(text: str, model: str = GPT_MODEL) -> int:
-    """Return the number of tokens in a string."""
-    encoding = tiktoken.encoding_for_model(model)
-    return len(encoding.encode(text))
 
 async def async_query_summary(
     query: str,
     df: pd.DataFrame,
     model: str,
-    token_budget: int
+    token_budget: int,
+    min_n: int,
+    top_n: int
 ) -> str:
     #Return a message for GPT, with relevant source texts pulled from a dataframe.
-    ids, relatednesses = ids_ranked_by_relatedness_e5(query, df)
+    ids, relatednesses = ids_ranked_by_relatedness_e5(query, df, min_n=min_n, top_n=top_n)
     #Basically giving the most relevant IDs from the previous function
     introduction = introduction_prompt
     query_user = f"{query}"
@@ -108,7 +105,7 @@ async def async_query_summary(
     for id, result in zip(ids, results):
         job_description_summary, cost, elapsed_time = result
         
-        # Append the summary to the list
+        # Append summary to the list
         job_summaries.append({
             "id": id,
             "summary": job_description_summary
@@ -130,24 +127,29 @@ async def async_query_summary(
 async def ask(
     #This query is your question, only parameter to fill in function
     query: str,
+    min_n: int,
+    top_n: int,
     df: pd.DataFrame = df,
     model: str = GPT_MODEL,
     token_budget: int = 8192,
-    log_gpt_messages: bool = True
+    log_gpt_messages: bool = True,
 ) -> str:
     #Answers a query using GPT and a dataframe of relevant texts and embeddings.
-    query_user, job_id_description, job_summaries, total_cost_summaries = await async_query_summary(query, df, model=model, token_budget=token_budget)
+    query_user, job_id_description, job_summaries, total_cost_summaries = await async_query_summary(query, df, model=model, token_budget=token_budget, min_n=min_n, top_n=top_n)
 
-    #Save summaries in a df & then parquet
+    #Save summaries in a df & then parquet -> append data if function called more than once
     df_summaries = pd.DataFrame(job_summaries)
-    df_to_parquet(df_summaries, "summaries")
+    #logging.info(df_summaries)
+    #df_summaries.to_parquet(SAVE_PATH+ f'/summaries.parquet', engine='pyarrow')
 
+    append_parquet(df_summaries, 'summaries')
     
     messages = [
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": f"{delimiters}{query_user}{delimiters}"},
         {"role": "assistant", "content": job_id_description}
     ]
+    
     if log_gpt_messages:
         logging.info(messages)
     response = openai.ChatCompletion.create(
@@ -161,7 +163,7 @@ async def ask(
     total_tokens = response['usage']['total_tokens']
     prompt_tokens = response['usage']['prompt_tokens']
     completion_tokens = response['usage']['completion_tokens']
-    logging.info(f"OPERATION: GPT-3.5 TURBO SUMMARISING. \n TOTAL COST: {total_cost_summaries}")
+    logging.info(f"\nOPERATION: GPT-3.5 TURBO SUMMARISING. \nTOTAL COST: ${total_cost_summaries} USD")
     #logging.info(f"OPERATION: {GPT_MODEL} CLASSIFYING \nPROMPT TOKENS USED:{prompt_tokens}\n COMPLETION TOKENS USED:{completion_tokens}\n \nTOTAL TOKENS USED:{total_tokens}\n)
 
     #Approximate cost
@@ -169,36 +171,36 @@ async def ask(
         prompt_cost = round((prompt_tokens / 1000) * 0.03, 3)
         completion_cost = round((completion_tokens / 1000) * 0.06, 3)
         cost_classify = prompt_cost + completion_cost
-        logging.info(f"OPERATION: {GPT_MODEL} CLASSIFICATION \nPROMPT TOKENS USED:{prompt_tokens}\n COMPLETION TOKENS USED:{completion_tokens}\n \nTOTAL TOKENS USED:{total_tokens}\n COST FOR CLASSIFYING: ${cost_classify} USD")
+        logging.info(f"\nOPERATION: {GPT_MODEL} CLASSIFICATION \nPROMPT TOKENS USED:{prompt_tokens}\nCOMPLETION TOKENS USED:{completion_tokens}\nTOTAL TOKENS USED:{total_tokens}\nCOST FOR CLASSIFYING: ${cost_classify} USD")
     elif GPT_MODEL == "gpt-3.5-turbo":
         prompt_cost = round((prompt_tokens / 1000) * 0.0015, 3)
         completion_cost = round((completion_tokens / 1000) * 0.002, 3)
         cost_classify = prompt_cost + completion_cost
-        logging.info(f"OPERATION: {GPT_MODEL} CLASSIFICATION \nPROMPT TOKENS USED:{prompt_tokens}\n COMPLETION TOKENS USED:{completion_tokens}\n \nTOTAL TOKENS USED:{total_tokens}\n COST FOR CLASSIFYING: ${cost_classify} USD")
+        logging.info(f"\nOPERATION: {GPT_MODEL} CLASSIFICATION \nPROMPT TOKENS USED:{prompt_tokens}\nCOMPLETION TOKENS USED:{completion_tokens}\nTOTAL TOKENS USED:{total_tokens}\nCOST FOR CLASSIFYING: ${cost_classify} USD")
     elif GPT_MODEL == "gpt-3.5-turbo-16k":
         prompt_cost = round((prompt_tokens / 1000) * 0.003, 3)
         completion_cost = round((completion_tokens / 1000) * 0.004, 3)
         cost_classify = prompt_cost + completion_cost
-        logging.info(f"OPERATION: {GPT_MODEL} CLASSIFICATION \nPROMPT TOKENS USED:{prompt_tokens}\n COMPLETION TOKENS USED:{completion_tokens}\n \nTOTAL TOKENS USED:{total_tokens}\n COST FOR CLASSIFYING: ${cost_classify} USD")
+        logging.info(f"\nOPERATION: {GPT_MODEL} CLASSIFICATION \nPROMPT TOKENS USED:{prompt_tokens}\nCOMPLETION TOKENS USED:{completion_tokens}\nTOTAL TOKENS USED:{total_tokens}\nCOST FOR CLASSIFYING: ${cost_classify} USD")
 
     #relatednesses
-    ids, relatednesses = ids_ranked_by_relatedness_e5(query=query, df=df)
+    ids, relatednesses = ids_ranked_by_relatedness_e5(query=query, df=df, min_n=min_n, top_n=top_n)
     for id, relatedness in zip(ids, relatednesses):
         logging.info(f"ID: {id} has the following {relatedness=:.3f}")
     
-    elapsed_time = timeit.default_timer() - start_time
-    logging.info(f"\n DreamedJobAI finished! all in: {elapsed_time:.2f} seconds \n")
+    elapsed_time = (timeit.default_timer() - start_time) / 60
+    logging.info(f"\nGPT-3.5 TURBO & GPT-4 finished summarising and classifying! all in: {elapsed_time:.2f} minutes \n")
     
     return response_message
 
-async def check_output_GPT4(input_cv: str) -> str:
+async def check_output_GPT4(input_cv: str, min_n:int, top_n:int) -> str:
     default = '[{"id": "", "suitability": "", "explanation": ""}]'
     default_json = json.loads(default)
     
     for _ in range(6):
         i = _ + 1
         try:
-            python_string = await ask(input_cv)
+            python_string = await ask(query=input_cv, min_n=min_n, top_n=top_n)
             try:
                 data = json.loads(python_string)
                 logging.info(f"Response is a valid json object. Done in loop number: {i}")
@@ -217,32 +219,64 @@ async def check_output_GPT4(input_cv: str) -> str:
     logging.error("Check logs!!!! Main function was not callable. Setting json to default")
     return default_json
 
+#Modify df options
+set_dataframe_display_options()
 
 async def main():
+    min_n=0
+    top_n=10
 
+    # Define the suitable categories
+    suitable_categories = ['Highly Suitable', 'Moderately Suitable', 'Potentially Suitable']
 
-    checked_json = await check_output_GPT4(abstract_cv)
+    # Initialize the dataframe
+    df_appended = pd.DataFrame()
 
-    def ids_json_loads(data: list[dict[str, str, str]] = None) -> str:
-        if data is None:
-            data = checked_json
-            logging.info(f"type of the json object: {type(data)} Data: {data}")
-            #print(type(exp), exp)
+    # Continue to call the function until we have 10 suitable jobs
+    counter = 0
+    while True:
+        checked_json = await check_output_GPT4(input_cv=abstract_cv, min_n=min_n, top_n=top_n)
         
+        # Convert the JSON to a dataframe and append it to the existing dataframe
+        df_original = pd.read_json(json.dumps(checked_json))
+        df_appended = pd.concat([df_appended, df_original], ignore_index=True)
+        
+        counter += 1
+        logging.info(f"Looking for suitable jobs. Current loop: {counter}")
+
+        logging.info(f"Current min_n: {min_n}. Current top_n: {top_n}")
+
+        # Increment the counters
+        min_n += 10
+        top_n += 10
+
+        # Filter the dataframe to only include the suitable jobs
+        df_most_suitable = df_appended[df_appended['suitability'].isin(suitable_categories)] if 'suitability' in df_appended.columns else pd.DataFrame()
+        
+        df_appended.to_parquet(SAVE_PATH + f"/df_appended.parquet", index=False)
+        df_most_suitable.to_parquet(SAVE_PATH + f"/df_most_suitable.parquet", index=False)
+
+        # Break the loop if we have 10 suitable jobs
+        if len(df_most_suitable) >= 10:
+            break
+
+    logging.info(f"\nDF APPENDED:\n{df_appended}")
+    
+    #Get the ids
+    def ids_df_most_suitable(df: pd.DataFrame = df_most_suitable) -> str:
         ids = ""
-        for item in data:
-            if "id" in item:
+        for _, row in df.iterrows():
+            if "id" in row:
                 if ids:
                     ids += ", "
-                ids += f"'{item['id']}'"
+                ids += f"'{row['id']}'"
 
         return f"({ids})"
 
-    ids_ready = ids_json_loads()
-    logging.info(f"Getting the ids from the json object: {type(ids_ready)}, {ids_ready}")
+    ids_most_suitable = ids_df_most_suitable()
+    logging.info(f"Getting the ids from the json object: {type(ids_most_suitable)}, {ids_most_suitable}")
 
-
-    def find_jobs_per_ids(ids:str, table: str = "test") -> pd.DataFrame:
+    def find_jobs_per_ids(ids:str, table: str = "main_jobs") -> pd.DataFrame:
         conn = psycopg2.connect(user=user, password=password, host=host, port=port, database=database)
         # Create a cursor object
         cur = conn.cursor()
@@ -264,45 +298,29 @@ async def main():
             'link': all_links,
             'location': all_locations
         })
-
-
-        # Close the database connection
+                # Close the database connection
         cur.close()
         conn.close()
 
         return df
 
-    df_postgre = find_jobs_per_ids(ids=ids_ready)
-    #Read the parquet
+    df_postgre = find_jobs_per_ids(ids=ids_most_suitable)
+
+    #Read the parquet with ids & summaries
     df_summaries = pd.read_parquet(SAVE_PATH + "/summaries.parquet")
+    #Merge it with the data in postgre
+    df_postgre_summaries = df_postgre.merge(df_summaries, on='id', how='inner')
+    #Merge with most suitable df so you have all the rows
+    df = df_postgre_summaries.merge(df_most_suitable, on="id", how='inner')
 
-    df = df_postgre.merge(df_summaries, on='id', how='inner')
+    logging.info(f"\nALL ROWS:\n{df}")
 
-    logging.info(f"RELATED JOBS & THEIR SUMMARIES: \n {df}")
-
-    def adding_all_data(df: pd.DataFrame, suitable_jobs: list) -> pd.DataFrame:
-        for index, row in df.iterrows():
-            entry_id = row['id']
-            for json_item in suitable_jobs:
-                if int(json_item['id']) == entry_id:
-                    suitability = json_item['suitability']
-                    explanation = json_item['explanation']
-                    df.at[index, 'suitability'] = suitability
-                    df.at[index, 'explanation'] = explanation
-                    break
-        return df
-
-    updated_data = adding_all_data(df=df, suitable_jobs=checked_json)
-
-    logging.info(f"ALL COLUMNS: \n {updated_data}")
 
     def sort_df_by_suitability(df: pd.DataFrame = df) -> pd.DataFrame:
         custom_order = {
             'Highly Suitable': 1,
             'Moderately Suitable': 2,
-            'Potentially Suitable': 3,
-            'Marginally Suitable': 4,
-            'Not Suitable': 5
+            'Potentially Suitable': 3
         }
         df['suitability_rank'] = df['suitability'].map(custom_order)
         sorted_df = df.sort_values(by='suitability_rank')
@@ -315,9 +333,8 @@ async def main():
     
     sorted_df.to_parquet(SAVE_PATH + f"{filename}.parquet", index=False)
 
+    logging.info(f"\nSORTED DF:\n{sorted_df}.\n\nThis df has been saved in ...{filename}.parquet\n\n\n")
 
-    logging.info(f"SORTED DF:\n {sorted_df}. \n This df has been saved in ...{filename}.parquet")
 
 if __name__ == "__main__":
 	asyncio.run(main())
-        
