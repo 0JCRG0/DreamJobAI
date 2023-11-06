98b3d6de19c0bbfeb0317e46218504460f30ff55
JCRG <juancarlosrg1999@gmail.com>
2023-11-06 16:59:47 -0600

    replacing parquet forpgvector

--- EmbeddingsE5.py
+++ EmbeddingsE5.py
@@ -1,5 +1,4 @@
 import torch.nn.functional as F
-
 import torch
 from torch import Tensor
 from torch.utils.data import DataLoader, Dataset
@@ -7,39 +6,22 @@ from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel
 import pretty_errors
 import pandas as pd
-from datetime import datetime
-import pyarrow as pa
-import pyarrow.parquet as pq
 import numpy as np
 from utils.handy import LoggingMain, append_parquet
 import pretty_errors
-from dotenv import load_dotenv
-import os
-from tenacity import (
-    retry,
-    stop_after_attempt,
-    wait_random_exponential,
-)  # for exponential backoff
 
-
-load_dotenv('.env')
-SAVE_PATH = os.getenv("SAVE_PATH")
-E5_BASE_PREEXISTING_JOBS = os.getenv("E5_BASE_PREEXISTING_JOBS")
-E5_BASE_TODAY_JOBS = os.getenv("E5_BASE_RECENT_JOBS")
-E5_BASE_TOTAL_JOBS = os.getenv("E5_BASE_TOTAL_JOBS")
 
 LoggingMain()
 
 #@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
-def embedding_e5_base_v2(batches_to_embed: list[str], batches_ids: list[str], batches_locations: list[str], original_timestamps: list[str], chunk_size: int) -> list:
+def embeddings_e5_base_v2_to_df(batches_to_embed: list[str], jobs_info: list[str], batches_ids: list[str]) -> pd.DataFrame:
     
-    INPUT_TEXT = batches_to_embed
-    INPUT_IDS = batches_ids
-    INPUT_LOCATIONS = batches_locations
-    INPUT_TIMESTAMPS = original_timestamps
+    CHUNK_SIZE = 15
+    FORMATTED_E5_QUERY_BATCHES = batches_to_embed
+    JOBS_INFO_BATCHES = jobs_info
+    IDS = batches_ids
     TOKENIZER = AutoTokenizer.from_pretrained("intfloat/e5-base-v2")
     MODEL = AutoModel.from_pretrained("intfloat/e5-base-v2")
-    CHUNK_SIZE = chunk_size
 
     def average_pool(last_hidden_states: Tensor,
                     attention_mask: Tensor) -> Tensor:
@@ -62,7 +44,7 @@ def embedding_e5_base_v2(batches_to_embed: list[str], batches_ids: list[str], ba
         return batch_dict
 
     # Create a dataset and a dataloader
-    dataset = TextDataset(INPUT_TEXT, TOKENIZER)
+    dataset = TextDataset(FORMATTED_E5_QUERY_BATCHES, TOKENIZER)
     dataloader = DataLoader(dataset, batch_size=CHUNK_SIZE, collate_fn=lambda b: collate_fn(b, TOKENIZER))
 
     # Process the data using the dataloader
@@ -80,25 +62,18 @@ def embedding_e5_base_v2(batches_to_embed: list[str], batches_ids: list[str], ba
     # Concatenate embeddings and save to a single Parquet file
     EMBEDDINGS = np.vstack(embeddings_list)
 
-    #Timestamps
-    """
-    TIMESTAMPS = []
-    timestamp = datetime.now()
-    TIMESTAMPS.extend([timestamp] * len(INPUT_IDS)) """
 
     df_data = {
-        'id': INPUT_IDS,
-        'description': INPUT_TEXT,
-        'location': INPUT_LOCATIONS,
+        'id': IDS,
+        'job_info': JOBS_INFO_BATCHES,
         'embedding': list(EMBEDDINGS),
-        'timestamp': INPUT_TIMESTAMPS
         }
 
-    new_data = pd.DataFrame(df_data)
+    df = pd.DataFrame(df_data)
 
-    #new_data.to_parquet(SAVE_PATH + f'/e5_base_v2_data.parquet', engine='pyarrow')
+    return df
 
-    append_parquet(new_data, "e5_base_v2_data")
+
 
 
 
